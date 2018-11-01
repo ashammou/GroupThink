cb8970a80ca4aa14294ffe3513e8348e2b1157d2
ashammou <ashammou@umich.edu>
2018-10-31 21:00:38 -0400

    Addition of UI (Blank)

---
+++ app/src/main/java/com/example/alishammout/groupthink/MeetingInformation.java
@@ -0,0 +1,13 @@
+package com.example.alishammout.groupthink;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MeetingInformation extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meeting_information);
+    }
+}
