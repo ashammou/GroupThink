358c0da4bbfed6c08f927e6b284b874248cebe9f
ashammou <ashammou@umich.edu>
2018-11-27 13:17:22 -0500

    Default Constructor + Full Constructor set up with ArrayLists for different all classes including get and set methods for each

---
+++ app/src/main/java/com/example/alishammout/groupthink/GroupMemberClass.java
@@ -0,0 +1,49 @@
+package com.example.alishammout.groupthink;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import java.util.*;
+
+
+public class GroupMemberClass extends Activity{
+
+    public List<UserClass> usersinGroupL;
+    public List<MeetingClass> meetingsinGroupL;
+
+    public GroupMemberClass() {
+
+        usersinGroupL = new ArrayList<>();
+        meetingsinGroupL = new ArrayList<>();
+
+    }
+
+    public GroupMemberClass(List<UserClass> usersinGroup, List<MeetingClass> meetingsinGroup) {
+
+        usersinGroupL = new ArrayList<>(usersinGroup);
+        meetingsinGroupL = new ArrayList<>(meetingsinGroup);
+
+    }
+
+    public List<UserClass> getUsersinGroup() {
+        return usersinGroupL;
+    }
+
+    public List <MeetingClass> getMeetingsinGroup() {
+        return meetingsinGroupL;
+    }
+
+    public void setUsersinGroup(List<UserClass> usersinGroup) {
+        usersinGroupL = usersinGroup;
+    }
+
+    public void setMeetingsinGroup(List<MeetingClass> meetingsinGroup) {
+        meetingsinGroupL = meetingsinGroup;
+    }
+
+
+
+
+
+
+}
