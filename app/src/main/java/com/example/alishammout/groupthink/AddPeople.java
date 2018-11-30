82176d90c5a9a61578bd56e8766ddfb6174d25fc
Griffin Haymes <ghaymes@umich.edu>
2018-11-30 15:16:15 -0500

    Added back button functionality to MeetingSelection activity, and back button and cancel button functionality to AddPeople activity

--- app/src/main/java/com/example/alishammout/groupthink/AddPeople.java
+++ app/src/main/java/com/example/alishammout/groupthink/AddPeople.java
@@ -1,13 +1,37 @@
 package com.example.alishammout.groupthink;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AddPeople extends Activity {
+public class AddPeople extends Activity implements View.OnClickListener {
+
+    private Button doneButton;
+    private Button cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_people);
+
+        doneButton = findViewById(R.id.doneButton);
+        cancelButton = findViewById(R.id.cancelButton);
+
+        doneButton.setOnClickListener(this);
+        cancelButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == doneButton) {
+            Intent intent = new Intent(AddPeople.this, MeetingSelection.class);
+            startActivity(intent);
+        }
+        else if (view == cancelButton) {
+            Intent intent = new Intent(AddPeople.this, GroupOverview.class);
+            startActivity(intent);
+        }
     }
 }
