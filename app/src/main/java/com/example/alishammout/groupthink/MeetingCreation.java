015480eca6a3ef43b8b198bb2c95ce332e15ffda
Marius <goffart@umich.edu>
2018-11-30 13:43:52 -0500

    meeting creation base

--- app/src/main/java/com/example/alishammout/groupthink/MeetingCreation.java
+++ app/src/main/java/com/example/alishammout/groupthink/MeetingCreation.java
@@ -1,13 +1,42 @@
 package com.example.alishammout.groupthink;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MeetingCreation extends Activity {
+public class MeetingCreation extends Activity implements View.OnClickListener{
+
+    private TextView textViewTitle;
+    private EditText editTextMeetingName, editTextMeetingTime, editTextLocation;
+    private Button buttonAddPeople;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meeting_creation);
+
+        textViewTitle = findViewById(R.id.textViewTitle);
+        editTextLocation = findViewById(R.id.editTextLocation);
+        editTextMeetingName = findViewById(R.id.editTextMeetingName);
+        editTextMeetingTime = findViewById(R.id.editTextMeetingTime);
+
+        buttonAddPeople.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == buttonAddPeople) {
+            
+            //has to save all entered data before switching
+            Intent intent = new Intent(
+                    MeetingCreation.this, AddPeople.class);
+            startActivity(intent);
+        }
+
     }
 }
